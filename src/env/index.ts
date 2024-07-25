100a21f74c6d02022e0bc3dbac3d93cf61f5dc0e
habini86 <vinicius1234585@gmail.com>
2024-07-25 08:17:21 -0300

    ci: corrigindo env

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  JWT_SECRET: z.string(),
+  JWT_SECRET: z.string().default('secret'),
   NUMBER_PASS_HASH: z.coerce.number().default(6),
 })
 
